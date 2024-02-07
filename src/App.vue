5a1c22dc615def9a7633aa3847c3edca07ca821d
MatteoCantafio25 <mattyteo004@gmail.com>
2024-02-07 21:15:30 +0100

    milestone 1 completed

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <script>
 import axios from 'axios';
-const endpointMovie = "https://api.themoviedb.org/3/search/movie";
+import { store } from '@/components/data/store.js';
+import { apiKey } from './components/data/index.js';
+import { endpoint } from './components/data/index.js';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 export default {
   name: "Vite Boolflix",
+  components: {
+    AppHeader,
+    AppMain,
+  },
+
+  methods: {
+    fetchMovies(endpoint) {
+      axios.get(endpoint).then(res => {
+        store.movies = res.data.results;
+      })
+    },
+    searchMovies(term) {
+      console.log("devo cercare...", term);
+      const searchEndpointMovie = `${endpoint}?api_key=${apiKey}&query=${term}&language=it-IT`;
+      this.fetchMovies(searchEndpointMovie);
+      console.log(searchEndpointMovie);
+    }
+  },
+
 };
 </script>
 
 <template>
-  <!-- Template Here -->
+  <AppHeader @search-movies="searchMovies" />
+  <AppMain />
 </template>
 
 <style lang="scss">
---
+++ src/components/AppMain.vue
@@ -0,0 +1,22 @@
+<script>
+import MoviesList from './movies/MoviesList.vue';
+export default {
+    name: "AppMain",
+    components: {
+        MoviesList,
+    }
+};
+</script>
+
+<template>
+    <main class="pt-5">
+        <MoviesList />
+    </main>
+</template>
+
+<style lang="scss" scoped >
+main {
+    background-color: #434343;
+    min-height: 90vh;
+}
+</style>
